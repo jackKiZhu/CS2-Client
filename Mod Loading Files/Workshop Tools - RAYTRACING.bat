f96e57d2eef5527f83dd0a2c060faf0ce6c6cfa1
NeXi2k <FreeUA3@outlook.de>
2023-07-05 18:17:10 +0200

    Fixed TraverseDirectoryAndWatchForChanges Bug for Workshop Tools

--- Mod Loading Files/Workshop Tools - RAYTRACING.bat
+++ Mod Loading Files/Workshop Tools - RAYTRACING.bat
@@ -1,5 +1,6 @@
 @echo off
 color 6
+if not exist "./content/csgo_mods" mkdir "./content/csgo_mods"
 echo Starting CS:2 Workshop Tools - Raytracing Mode...
 start "" "game\bin\win64\csgocfg.exe"
 timeout /t 5 /nobreak
